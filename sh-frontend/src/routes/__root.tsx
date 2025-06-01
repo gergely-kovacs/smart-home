a0c00ed247cf2810d7af2a36ee37d6afbe434482
Gergely Kovacs <gergely.kovacs@hotmail.com>
2025-06-01 14:02:18 +0200

    initialized the frontend project

---
+++ sh-frontend/src/routes/__root.tsx
@@ -0,0 +1,15 @@
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+import Header from "../components/Header";
+
+export const Route = createRootRoute({
+  component: () => (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  ),
+});
